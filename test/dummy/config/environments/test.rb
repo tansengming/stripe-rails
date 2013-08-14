7c0c1a07253ab196bf5cf4dddd3443dfd04cba3e
Michael Herold <michael.j.herold@gmail.com>
2013-08-14 17:25:53 -0400

    Set eager_load to false in test env.

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -35,4 +35,7 @@ Dummy::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Set eager_load to false as it's unneeded
+  config.eager_load = false
 end
