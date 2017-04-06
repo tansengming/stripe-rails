81aa88f7b410b66f35c196901092ffb4fa40a713
SengMing Tan <tansengming@gmail.com>
2017-04-06 11:01:00 -0400

    spec that v3 debug is not used

--- test/javascript_helper_spec.rb
+++ test/javascript_helper_spec.rb
@@ -13,4 +13,16 @@ class Stripe::JavascriptHelperTest < ActionView::TestCase
   test "should render v3 if v3 option is passed" do
     assert_includes stripe_javascript_tag(:v3), 'https://js.stripe.com/v3/'
   end
+
+  test 'should render debug js if debug flag is enabled' do
+    Rails.application.config.stripe.debug_js = true
+
+    assert_includes stripe_javascript_tag, 'https://js.stripe.com/v1/stripe-debug.js'
+  end
+
+  test 'should not render debug js if debug flag is enabled and v3 is selected' do
+    Rails.application.config.stripe.debug_js = true
+
+    assert_includes stripe_javascript_tag(:v3), 'https://js.stripe.com/v3/'
+  end
 end
