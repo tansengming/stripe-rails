73d9ed816d515f2fbd31efe44179016daa34db39
SengMing Tan <tansengming@gmail.com>
2017-04-06 11:10:28 -0400

    removes _debug_js partial

--- app/helpers/stripe/javascript_helper.rb
+++ app/helpers/stripe/javascript_helper.rb
@@ -3,16 +3,7 @@ module Stripe
     def stripe_javascript_tag(stripe_js_version = 'v1')
       stripe_js_version = stripe_js_version.to_s
 
-      case stripe_js_version
-      when 'v1', 'v2'
-        if ::Rails.application.config.stripe.debug_js
-          render 'stripe/debug_js', stripe_js_version: stripe_js_version
-        else
-          render 'stripe/js', stripe_js_version: stripe_js_version
-        end
-      when 'v3' # the debug js for v3 isn't available
-        render 'stripe/js', stripe_js_version: stripe_js_version
-      end
+      render 'stripe/js', stripe_js_version: stripe_js_version
     end
   end
 end
