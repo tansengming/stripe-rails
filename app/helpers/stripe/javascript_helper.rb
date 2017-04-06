abdb445b3663e22c79e8771b6980d83b9a399089
SengMing Tan <tansengming@gmail.com>
2017-04-06 10:39:50 -0400

    supports multiple versions of the stripe JS

--- app/helpers/stripe/javascript_helper.rb
+++ app/helpers/stripe/javascript_helper.rb
@@ -1,7 +1,18 @@
 module Stripe
   module JavascriptHelper
-    def stripe_javascript_tag
-      render :partial => 'stripe/js'
+    def stripe_javascript_tag(stripe_js_version = 'v1')
+      stripe_js_version = stripe_js_version.to_s
+
+      case stripe_js_version
+      when 'v1', 'v2'
+        if ::Rails.application.config.stripe.debug_js
+          render 'stripe/debug_js', stripe_js_version: stripe_js_version
+        else
+          render 'stripe/js', stripe_js_version: stripe_js_version
+        end
+      when 'v3' # no debug js for v3
+        render 'stripe/js', stripe_js_version: stripe_js_version
+      end
     end
   end
 end
