3fc2934465041519602adf45f2dcca108cc75f72
SengMing Tan <tansengming@gmail.com>
2017-07-21 14:49:46 -0400

    updates specs to run with new defaults

--- test/javascript_helper_spec.rb
+++ test/javascript_helper_spec.rb
@@ -12,21 +12,21 @@ describe Stripe::JavascriptHelper do
 
   describe '#stripe_javascript_tag' do
     describe 'when no options are passed' do
-      it 'should default to v1' do
-        @view.stripe_javascript_tag.must_include 'https://js.stripe.com/v1/'
+      it 'should default to v3' do
+        @view.stripe_javascript_tag.must_include 'https://js.stripe.com/v3/'
       end
     end
 
-    describe 'when the v3 option is passed' do
-      it 'should default to v3' do
-        @view.stripe_javascript_tag(:v3).must_include 'https://js.stripe.com/v3/'
+    describe 'when the v2 option is passed' do
+      it 'should default to v2' do
+        @view.stripe_javascript_tag(:v2).must_include 'https://js.stripe.com/v2/'
       end
     end
 
     describe 'when the debug flag is enabled' do
       before { Rails.application.config.stripe.debug_js = true }
       it 'should render the debug js' do
-        @view.stripe_javascript_tag.must_include 'https://js.stripe.com/v1/stripe-debug.js'
+        @view.stripe_javascript_tag(:v1).must_include 'https://js.stripe.com/v1/stripe-debug.js'
       end
 
       describe 'when v3 is selected' do
