315edcadf8a24abdf6407007d515ace5dad68132
SengMing Tan <tansengming@gmail.com>
2017-08-07 17:02:47 -0400

    don’t run system test if not supported

--- test/dummy_stripes_controller_spec.rb
+++ test/dummy_stripes_controller_spec.rb
@@ -1,12 +1,18 @@
-class DummyStripesControllerSpec < ApplicationSystemTestCase
-  setup do
-    Dummy::Application.configure do
-      config.stripe.publishable_key = 'pk_test_XXXYYYZZZ'
+begin
+  require 'application_system_test_case'
+
+  class DummyStripesControllerSpec < ApplicationSystemTestCase
+    setup do
+      Dummy::Application.configure do
+        config.stripe.publishable_key = 'pk_test_XXXYYYZZZ'
+      end
+    end
+
+    test "visiting the index" do
+      visit new_stripe_url
+      assert_text 'This page tests the loading and initialization of Stripe JS'
     end
   end
-
-  test "visiting the index" do
-    visit new_stripe_url
-    assert_text 'This page tests the loading and initialization of Stripe JS'
-  end
+rescue NameError
+  warn 'WARNING: System test was skipped because this Rails version does not support it!'
 end
