07c51bc6420e34f6a84f020fd8c1e7ff91946230
SengMing Tan <tansengming@gmail.com>
2017-08-08 09:23:40 -0400

    also test loading of v2 version of stripe JS

--- test/dummy_stripes_controller_spec.rb
+++ test/dummy_stripes_controller_spec.rb
@@ -8,8 +8,13 @@ begin
       end
     end
 
-    test "visiting the index" do
+    test "loading the default javascript helper" do
       visit new_stripe_url
+      assert_text 'This page tests the loading and initialization of Stripe JS'
+    end
+
+    test "loading the v2 version of the javascript helper" do
+      visit new_stripe_url(version: 'v2')
       assert_text 'This page tests the loading and initialization of Stripe JS'
     end
   end
