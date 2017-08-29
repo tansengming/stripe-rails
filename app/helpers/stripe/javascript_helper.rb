3c4133ba9d0134b061918e1dcb60d8c454a004b7
SengMing Tan <tansengming@gmail.com>
2017-08-29 12:47:16 -0400

    refactor

--- app/helpers/stripe/javascript_helper.rb
+++ app/helpers/stripe/javascript_helper.rb
@@ -1,7 +1,8 @@
 module Stripe
   module JavascriptHelper
-    DEFAULT_JS_VERSION = 'v3'
-    def stripe_javascript_tag(stripe_js_version = DEFAULT_JS_VERSION)
+    DEFAULT_STRIPE_JS_VERSION = 'v3'
+
+    def stripe_javascript_tag(stripe_js_version = DEFAULT_STRIPE_JS_VERSION)
       stripe_js_version = stripe_js_version.to_s.downcase
 
       render 'stripe/js', stripe_js_version: stripe_js_version
