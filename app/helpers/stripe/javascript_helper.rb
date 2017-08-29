905768fb01d7473cc2529950912dc2bcd2191439
SengMing Tan <tansengming@gmail.com>
2017-08-29 12:42:57 -0400

    specs for partial version of js helpers

--- app/helpers/stripe/javascript_helper.rb
+++ app/helpers/stripe/javascript_helper.rb
@@ -1,6 +1,7 @@
 module Stripe
   module JavascriptHelper
-    def stripe_javascript_tag(stripe_js_version = 'v3')
+    DEFAULT_JS_VERSION = 'v3'
+    def stripe_javascript_tag(stripe_js_version = DEFAULT_JS_VERSION)
       stripe_js_version = stripe_js_version.to_s.downcase
 
       render 'stripe/js', stripe_js_version: stripe_js_version
