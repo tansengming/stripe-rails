e26b8f92dc46b90cfb7f559e3e21f023421f0ff0
SengMing Tan <tansengming@gmail.com>
2018-07-16 16:38:52 -0400

    back to getting driven on headless

--- test/support/application_system_test_case.rb
+++ test/support/application_system_test_case.rb
@@ -4,5 +4,5 @@ require File.expand_path("null_system_test_case", __dir__)
 SystemTestCaseKlass = defined?(ActionDispatch::SystemTestCase) ? ActionDispatch::SystemTestCase : NullSystemTestCase
 
 class ApplicationSystemTestCase < SystemTestCaseKlass
-  # driven_by :selenium_chrome_headless
+  driven_by :selenium_chrome_headless
 end
