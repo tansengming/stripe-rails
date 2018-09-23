2bea4be5933c4117107d2f4523964db7d3faefcd
Jan Schwenzien <jan@general-scripting.com>
2018-09-23 08:58:57 +0200

    added statement_descriptor to product builder

--- lib/stripe/products.rb
+++ lib/stripe/products.rb
@@ -11,17 +11,23 @@ module Stripe
                     :caption,
                     :metadata,
                     :shippable,
-                    :url
+                    :url,
+                    :statement_descriptor
 
       validates_presence_of :name, :type
 
       validates :active, :shippable, inclusion: { in: [true, false] }, allow_nil: true
       validates :type, inclusion: { in: %w(service good) }
       validates :caption, :description, :shippable, :url, absence: true, unless: :good?
+      validates :statement_descriptor, absence: true, unless: :service?
 
       private
       def good?
         type == 'good'
+      end
+
+      def service?
+        type == 'service'
       end
 
       def create_options
@@ -35,6 +41,7 @@ module Stripe
           metadata: metadata,
           shippable: shippable,
           url: url,
+          statement_descriptor: statement_descriptor
         }.delete_if{|_, v| v.nil? }
       end
     end
