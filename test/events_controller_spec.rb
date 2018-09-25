e33c76c8c07ae3cf8993e6ee646010a0dea87891
Wyatt Kirby <wyatt@apsis.io>
2018-09-25 13:13:28 -0700

    Add tests for signature validation

--- test/events_controller_spec.rb
+++ test/events_controller_spec.rb
@@ -11,7 +11,7 @@ describe Stripe::EventsController do
   end
 
   describe 'the events interface' do
-    let(:params) { 
+    let(:params) {
       {
         id: 'evt_00000000000000',
         type: 'customer.updated',
@@ -21,5 +21,34 @@ describe Stripe::EventsController do
     subject { post '/stripe/events', params.to_json }
 
     it { subject.must_be :ok? }
+  end
+
+  describe 'signed webhooks' do
+    before do
+      header 'Stripe-Signature', 't=1537832721,v1=123,v0=123'
+    end
+
+    let(:params) {
+      {
+        id: 'evt_00000000000001',
+        type: 'customer.updated',
+        data: {
+          object: 'customer',
+          fingerprint: 'xxxyyyzzz'
+        },
+      }
+    }
+
+    subject { post '/stripe/events', params.to_json }
+
+    it 'returns bad_request when invalid' do
+      Stripe::Webhook.expects(:construct_event).raises(Stripe::SignatureVerificationError.new('msg', 'sig_header'))
+      subject.must_be :bad_request?
+    end
+
+    it 'returns ok when valid' do
+      Stripe::Webhook.expects(:construct_event).returns(Stripe::Event.construct_from(params))
+      subject.must_be :ok?
+    end
   end
 end
