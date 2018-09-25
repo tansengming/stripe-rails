0d2366f837a633b82cd54acb37262c04fd63247a
Wyatt Kirby <wyatt@apsis.io>
2018-09-25 13:13:20 -0700

    Update to use signature validation on webhooks

--- app/controllers/stripe/events_controller.rb
+++ app/controllers/stripe/events_controller.rb
@@ -4,8 +4,14 @@ module Stripe
     respond_to :json
 
     def create
-      @event = dispatch_stripe_event params
+      @event = dispatch_stripe_event(request)
       head :ok
+    rescue JSON::ParserError => e
+      ::Rails.logger.error e.message
+      head :bad_request, status: 400
+    rescue Stripe::SignatureVerificationError => e
+      ::Rails.logger.error e.message
+      head :bad_request, status: 400
     end
   end
 end
