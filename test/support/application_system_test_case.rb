b63cdeb075b6ead2cb457cfd56cb509656a22aea
SengMing Tan <tansengming@gmail.com>
2020-05-29 15:19:08 -0400

    removes rails 4 system test hacks

--- test/support/application_system_test_case.rb
+++ test/support/application_system_test_case.rb
@@ -1,8 +1,3 @@
-require File.expand_path("null_system_test_case", __dir__)
-
-# For Rails 4 compat
-SystemTestCaseKlass = defined?(ActionDispatch::SystemTestCase) ? ActionDispatch::SystemTestCase : NullSystemTestCase
-
-class ApplicationSystemTestCase < SystemTestCaseKlass
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium_chrome_headless
 end
